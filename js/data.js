29a60d3a67711a9257843a04bc482e483888f929
JoacDc <delcantodiegojoaquin5@gmail.com>
2026-05-07 19:57:31 -0300

    Primera prueba cloudinary

--- js/data.js
+++ js/data.js
@@ -1,8 +1,15 @@
+const CLOUDINARY_BASE = "https://res.cloudinary.com/dbdceppt7/image/upload/f_auto,q_auto,w_600/v1778191821/";
+
 const products = [
-    { id: 1, name: 'Blusa Floral Suave', price: 8900, category: 'remeras', image: 'https://images.unsplash.com/photo-1561677843-39dee7a319ca?w=800&q=80', tag: 'Nuevo', desc: 'Blusa de seda fría con caída natural.' },
-    { id: 2, name: 'Vestido Lino Midi', price: 14500, category: 'noche', image: 'https://images.unsplash.com/photo-1515372039744-b8f02a3ae446?w=800&q=80', tag: 'Favorito', desc: 'Vestido de lino premium corte midi.' },
-    { id: 3, name: 'Pantalón Wide Leg', price: 11200, category: 'pantalones', image: 'https://images.unsplash.com/photo-1594938298603-c8148c4dae35?w=800&q=80', tag: '', desc: 'Pantalón de tiro alto y pierna ancha.' },
-    { id: 4, name: 'Cardigan Camel', price: 13000, category: 'abrigos', image: 'https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=800&q=80', tag: 'Nuevo', desc: 'Tejido suave de punto fino.' },
-    { id: 5, name: 'Falda Plisada Rose', price: 9800, category: 'noche', image: 'https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?w=800&q=80', tag: '', desc: 'Falda con movimiento y brillo sutil.' },
-    { id: 6, name: 'Jean Oxford Celeste', price: 15500, category: 'jeans', image: 'https://images.unsplash.com/photo-1541099649105-f69ad21f3246?w=800&q=80', tag: 'Top', desc: 'Jean clásico elastizado.' }
-];
+    
+    {   
+        id: 1,
+        name: 'Jenas Elastizado Oxford gris',
+        price: 38.000,
+        category: 'Jeans',
+        image: CLOUDINARY_BASE +'WhatsApp_Image_2026-05-07_at_19.06.42_hvggsm.jpg',
+        tag: 'Nuevo',
+        desc: 'Confexion chica'
+    }    
+    
+    ];
