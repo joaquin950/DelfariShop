e1a6a83f60734f35e7c12c39a6b75ac627795c16
JoacDc <delcantodiegojoaquin5@gmail.com>
2026-05-07 20:11:46 -0300

    PantalonesJeans funcionando

--- js/data.js
+++ js/data.js
@@ -4,12 +4,49 @@ const products = [
     
     {   
         id: 1,
-        name: 'Jenas Elastizado Oxford gris',
-        price: 38.000,
-        category: 'Jeans',
+        name: 'Jeans Elastizado Oxford gris',
+        price: 38000,
+        category: 'jeans',
         image: CLOUDINARY_BASE +'WhatsApp_Image_2026-05-07_at_19.06.42_hvggsm.jpg',
         tag: 'Nuevo',
         desc: 'Confexion chica'
-    }    
+    },  
     
+    {
+        id: 2,
+        name: 'Jeans Elastizado',
+        price: 38000,
+        category: 'jeans',
+        image: CLOUDINARY_BASE +'otro3_grd6hd.jpg',
+        tag: 'Nuevo',
+        desc: 'Confexion chica'
+
+    },
+
+    {
+        id: 3,
+        name: 'Jeans Recto',
+        price: 38000,
+        category: 'jeans',
+        image: CLOUDINARY_BASE +'otro_bfvunx.jpg',
+        tag: 'Nuevo',
+        desc: 'Confexion chica'
+
+    },
+
+    {
+        id: 4,
+        name: 'Jeans elastizado azul',
+        price: 38000,
+        category: 'jeans',
+        image: CLOUDINARY_BASE +'otro2_d8fimu.jpg',
+        tag: 'Nuevo',
+        desc: 'Confexion chica-Wideleg'
+
+    }
+
+
+
+
+
     ];
